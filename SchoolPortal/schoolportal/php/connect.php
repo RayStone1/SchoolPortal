6963dc44c9f7cd8de531e8d758be699df8fb329c
RayStone <ruslanchupanov05@gmail.com>
2022-02-25 17:22:13 +0300

    update

--- SchoolPortal/schoolportal/php/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$mysql = new mysqli('schoolportal', 'root', '', 'school-portal');
-$mysql->set_charset("utf8");
--- SchoolPortal/schoolportal/php/exit.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-session_unset($_SESSION['id_user']);
-// header("Location: ".$_SERVER['HTTP_REFERER']);
