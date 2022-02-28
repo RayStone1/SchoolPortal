80f9bf5b64f9a730b5893d99edbf0389ea01700f
RayStone <99405831+RayStone1@users.noreply.github.com>
2022-02-28 13:53:42 +0300

    Delete SchoolPortal directory

--- SchoolPortal/journal.php
+++
@@ -1,7 +0,0 @@
-<?php require_once "blocks/header.php"?>
-<br>
-<br>
-<br>
-<center><img src="img/journal.png" alt=""></center>
-<br>
-<?php require_once "blocks/footer.php"?>
--- SchoolPortal/php/sendmsg.php
+++
@@ -1,4 +0,0 @@
-<?php
-$id_user=$_SESSION['id_user'];
-$msg=$_POST['msg'];
-$id_chat=
